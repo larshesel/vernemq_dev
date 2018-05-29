cb5a5cdcd0a2489938527a9f8b59b986fa7f7caa
Lars Hesel Christensen <lars@larshesel.dk>
2018-05-29 10:17:42 +0200

    Add on unsubscribe v1 hook

---
+++ src/on_unsubscribe_v1_hook.erl
@@ -0,0 +1,14 @@
+-module(on_unsubscribe_v1_hook).
+-include("vernemq_dev.hrl").
+
+-type unsub_modifiers() :: {topics, [topic()]}
+                         | {properties, properties()}.
+
+%% called as an 'all'-hook, return value is ignored
+-callback on_unsubscribe_v1(UserName      :: username(),
+                            SubscriberId  :: subscriber_id(),
+                            Topics        :: [Topic :: topic()],
+                            Properties    :: properties()) ->
+    ok |
+    {ok, [unsub_modifiers()]} |
+    next.
