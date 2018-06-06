053e270ad32558ce3f0a0df7a8c914d1a81c6af7
Lars Hesel Christensen <lars@larshesel.dk>
2018-06-06 14:19:17 +0200

    Use map for on_auth_v1 return values

--- src/on_auth_v1_hook.erl
+++ src/on_auth_v1_hook.erl
@@ -1,19 +1,25 @@
 -module(on_auth_v1_hook).
 -include("vernemq_dev.hrl").
 
--type auth_modifiers() :: {username, username()}
-                        | {properties, properties()}.
-
 -type reason_code() :: reason_code_name().
 
--type error_values() :: {reason_code, reason_code()}
-                      | {properties, properties()}.
+-type auth_modifiers() ::
+        #{
+           reason_code := reason_code(),
+           properties => properties()
+         }.
+
+-type error_values() ::
+        #{
+           reason_code := reason_code(),
+           properties => properties()
+         }.
 
 %% called as an all_till_ok - hook
 -callback on_auth_v1(Properties :: properties()) ->
-    {ok, [auth_modifiers()]} | %% corresponds to ?SUCCESS
-    {incomplete, properties()} | %% corresponds to ?CONTINUE_AUTHENTICATION
-    {error, [error_values()]} |
+    {ok, auth_modifiers()} |
+    {error, error_values()} |
+    {error, any()} | %% does not send a reply to the client!
     next.
 
 -export_type([auth_modifiers/0]).
