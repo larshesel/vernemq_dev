6149035c9e2fb62255f9365e67903218a1fc074c
Lars Hesel Christensen <lars@larshesel.dk>
2018-06-07 08:09:25 +0200

    Use map for on_deliver_v1 hook return values

--- src/on_deliver_v1_hook.erl
+++ src/on_deliver_v1_hook.erl
@@ -1,8 +1,11 @@
 -module(on_deliver_v1_hook).
 -include("vernemq_dev.hrl").
--type msg_modifier() :: {topic, topic()}
-                      | {payload, payload()}
-                      | {properties, properties()}.
+-type msg_modifier() ::
+        #{
+           topic => topic(),
+           payload => payload(),
+           properties => properties()
+         }.
 
 -callback on_deliver_v1(UserName      :: username(),
                         SubscriberId  :: subscriber_id(),
@@ -11,7 +14,7 @@
                         Payload       :: payload()) ->
     ok |
     {ok, Payload    :: payload()} |
-    {ok, Modifiers  :: [msg_modifier()]} |
+    {ok, Modifiers  :: msg_modifier()} |
     next.
 
 -export_type([msg_modifier/0]).
